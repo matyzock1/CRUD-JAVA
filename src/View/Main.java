f0bd9f1f0fef6de7182a145fa826039f39de336e
matysok1 <m.aravenaf96@gmail.com>
2023-06-14 22:05:13 -0400

    Suma ekisde

--- src/View/Main.java
+++ src/View/Main.java
@@ -3,5 +3,12 @@ package View;
 public class Main {
     public static void main(String[] args) {
         System.out.println("pico");
+
+        int a = 20, b = 10;
+
+        int suma = a + b;
+
+        System.out.println("suma = " + suma);
+
     }
 }
